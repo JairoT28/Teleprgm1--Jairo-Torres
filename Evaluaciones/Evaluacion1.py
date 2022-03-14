35b3e8a94585eb68960bc8c3f0ddc70e3e874f8c
JairoT28 <JairoT28>
2022-03-13 22:58:32 -0400

    realizado

--- Evaluaciones/Evaluacion1.py
+++ Evaluaciones/Evaluacion1.py
@@ -1,5 +1,5 @@
 ## Declaracion de Variable ##
-Nombre_completo = "Jairo Alejandro Torres Melendez"
+Nombre_completo = "Jairo Alejandro Torres Meléndez"
 
 ## Imprimir en Pantalla ##
 print("Hola Prog1, soy", Nombre_completo)
