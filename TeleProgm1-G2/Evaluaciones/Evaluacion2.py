ec332a9672cf8bc3de1852bdcafeff6d9dc264a5
JairoT28 <JairoT28>
2022-03-28 15:19:08 -0400

    actualizaciones

---
+++ TeleProgm1-G2/Evaluaciones/Evaluacion2.py
@@ -0,0 +1,14 @@
+vTupla1dato = ("65737065726f2073616c6972206269656e",)
+print("Mi tupla de un elemento dice:", vTupla1dato)
+
+
+vEdades = [2, 7, 58, 7, 45, 26, 10, 8, 56, 57, 97, 19, 11, 53, 3, 99, 62, 78, 29, 9, 37, 42, 56, 86, 28, 86, 95, 26, 49, 67, 21, 815, 67, 10, 58, 512, 24, 92, 89, 67, 53, 10, 9, 83, 1, 44, 10, 77, 98, 73, 57]
+
+vEdades.pop(6)
+vEdades.pop(32)
+vEdades.pop(39)
+vEdades.pop(43)
+
+for vIndice in vEdades:
+    
+    print("Las personas vacunas son:", vIndice)
