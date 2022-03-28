00f46cce8775074424a0db671225196ff9f0b5cf
JairoT28 <JairoT28>
2022-03-28 18:17:09 -0400

    ajustes

--- probando codigos.py
+++ probando codigos.py
@@ -1,15 +1,21 @@
+from site import venv
+
+
 vTupla1dato = ("unsolodato",)
 
-print(vTupla1dato)
+for vindice2 in vTupla1dato:
+
+print("")
+
 
 vEdades = [2, 7, 58, 7, 45, 26, 10, 8, 56, 57, 97, 19, 11, 53, 3, 99, 62, 78, 29, 9, 37, 42, 56, 86, 28, 86, 95, 26, 49, 67, 21, 815, 67, 10, 58, 512, 24, 92, 89, 67, 53, 10, 9, 83, 1, 44, 10, 77, 98, 73, 57]
-vEdades.pop(6)
-vEdades.pop(32)
-vEdades.pop(39)
-vEdades.pop(43)
+vBorrar10 = 10
 
-
+for vIndice in vEdades:
+    if(vIndice == vBorrar10):
+        vEdades.remove(vBorrar10)
 print(vEdades)
+
 
 
 
