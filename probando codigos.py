474de727f6f8f26de96a18f59fcf7065b7734166
Jairo Torres <100247074+JairoT28@users.noreply.github.com>
2022-05-16 13:00:38 -0400

    a

--- probando codigos.py
+++ probando codigos.py
@@ -1,11 +1,9 @@
-from site import venv
-
-
 vTupla1dato = ("unsolodato",)
 
 for vindice2 in vTupla1dato:
 
-print("")
+    print("mi tupla dice:", vindice2[:])
+
 
 
 vEdades = [2, 7, 58, 7, 45, 26, 10, 8, 56, 57, 97, 19, 11, 53, 3, 99, 62, 78, 29, 9, 37, 42, 56, 86, 28, 86, 95, 26, 49, 67, 21, 815, 67, 10, 58, 512, 24, 92, 89, 67, 53, 10, 9, 83, 1, 44, 10, 77, 98, 73, 57]
