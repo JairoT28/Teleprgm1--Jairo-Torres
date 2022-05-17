a4f715c6952e871891c8e55d89d439da7422ffcf
Jairo Torres <100247074+JairoT28@users.noreply.github.com>
2022-05-16 20:18:11 -0400

    evaluación

---
+++ TeleProgm1-G2/Evaluaciones/Evaluación5.4.py
@@ -0,0 +1,12 @@
+## STRING ##
+
+def centrado():
+    frase = 'Enhorabuena'
+    
+    print("valor original", frase)
+    print(len(frase))
+    print('{:^30}'.format(frase))
+
+    return
+
+centrado()
